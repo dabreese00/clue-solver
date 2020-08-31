3f248848195344978ca01fdd47dabeacc85706c5
David Breese <dabreese00@gmail.com>
2020-08-31 01:07:59 -0500

    Rewrite Clue game on new model

--- app/cluegame.py
+++ app/cluegame.py
@@ -1,55 +1,76 @@
 from enum import Enum
 
-class CluePersonCard(Enum):
-    COLONEL_MUSTARD = "Colonel Mustard"
-    MISS_SCARLET = "Miss Scarlet"
 
-class ClueWeaponCard(Enum):
-    ROPE = "Rope"
-    LEAD_PIPE = "Lead pipe"
+class ClueCardType(Enum):
+    PERSON = "Person"
+    WEAPON = "Weapon"
+    ROOM = "Room"
 
-class ClueRoomCard(Enum):
-    BILLIARD_ROOM = "Billiard Room"
-    BALLROOM = "Ballroom"
-
-class CluePlayer:
+class Player:
     def __init__(self, name, hand_size):
         self.name = name
         self.hand_size = hand_size
-        self.show = 0
-        self.card_knowledge = {}
-        for c in (CluePersonCard + ClueWeaponCard + ClueRoomCard):
 
-
-    def record_show(cards):
-        for card in cards:
-            self.potential_cards[card] = self.show
-        self.show += 1
-
-class ClueCard:
-    def __init__(self, name):
+class Card:
+    def __init__(self, name, card_type):
         self.name = name
+        self.card_type = card_type
 
-class ClueGame:
-    total_cards = 21
-    num_card_types = 3
+# A yes/no: Does this player have this card?
+# Created when this fact is known.
+class Have:
+    def __init__(self, player, card, have):
+        self.player = player
+        self.card = card
+        self.have = have
 
-    def __init__(self, players):
-        if ( sum(p.hand_size for p in players) == self.total_cards - self.num_card_types ):
-            self.players = players
-        else:
-            raise ValueError('Invalid player hand sizes')
+# A 4-way relationship between a player and 3 cards.
+# Created when a player shows a card to someone else.
+class Show:
+    def __init__(self, player, cards):
+        self.player = player
+        self.cards = cards
 
-    def __validate_show_pass(self, player_name, person, weapon, room):
-        if not ( player_name in ( p.name for p in self.players ) ):
-            raise ValueError('There is no such player as {} in this game'.format(player_name))
-        if not ( person in CluePersonCard and weapon in ClueWeaponCard and room in ClueRoomCard ):
-            raise ValueError('Invalid person, weapon, or room card.')
+class Game:
+    clue_cards = {
+        ClueCardType.PERSON: ["Colonel Mustard", "Miss Scarlet"],
+        ClueCardType.WEAPON: ["Rope", "Lead pipe"],
+        ClueCardType.ROOM: ["Billiard Room", "Ballroom"]
+        }
 
-    def record_pass(self, player_name, person, weapon, room):
-        self.__validate_show_pass(player_name, person, weapon, room)
-        return 'Player {} has passed'.format(player_name)
+    def __init__(self, myself, other_players):
+        self.myself = myself
+        self.players = other_players.copy()
+        self.players.append(myself)
 
-    def record_show(self, player_name, person, weapon, room):
-        self.__validate_show_pass(player_name, person, weapon, room)
-        return 'Player {} has shown'.format(player_name)
+        self.cards = []
+        for t in ClueCardType:
+            for c in self.clue_cards[t]:
+                self.cards.append(Card(c, t))
+
+        haves = []
+        shows = []
+
+    def record_have(self, player, card):
+        pass
+        # 0. Record the have.
+        # 1. Mark passes for all other players
+        # 2. If all the player's cards are accounted for, mark passes for all other cards.
+        # 3. If all but 1 card of this ClueCardType are accounted for, mark
+        #     passes for all players for the remaining card.
+        # 4. If the player has this card in any of their "shows", void those shows.
+
+    def record_pass(self, player, card):
+        pass
+        # 0. Record the pass.
+        # 1. If the passing player & card are in any Shows together, check if
+        #    the Shows have "popped".  If so, record a Have and void the Show.
+
+    def record_show(self, player, card):
+        pass
+        # 0. Record the show.
+        # 1. Check if the Show has "popped".  If so, record a Have and void the Show.
+
+    def input_hand(self, cards):
+        for c in cards:
+            record_have(self.myself, c)
--- tests/test_cluegame.py
+++ tests/test_cluegame.py
@@ -1,30 +1,26 @@
-from app.cluegame import CluePersonCard, ClueWeaponCard, ClueRoomCard, CluePlayer, ClueGame
+from app.cluegame import ClueCardType, Card, Player, Have, Show, Game
 import pytest
 
 def test_one():
-    with pytest.raises(ValueError):
-        david = CluePlayer("David", 3)
-        adam = CluePlayer("Adam", 3)
-        players = [david, adam]
+    hand_size = 3
+    other_player_names = [ 'Adam', 'Cynthia', 'Greg' ]
+    other_players = []
+    for p in other_player_names:
+        other_players.append(Player(p, hand_size))
+    myself = Player('David', hand_size)
 
-        game = ClueGame(players)
+    game = Game(myself, other_players)
+
+    assert len(game.cards) > 2
 
 def test_two():
-    players = []
-    for n in ['a', 'b', 'c', 'd', 'e', 'f']:
-        players.append(CluePlayer(n, 3))
+    hand_size = 3
+    other_player_names = [ 'Adam', 'Cynthia', 'Greg' ]
+    other_players = []
+    for p in other_player_names:
+        other_players.append(Player(p, hand_size))
+    myself = Player('David', hand_size)
 
-    game = ClueGame(players)
-    assert game.players == players
+    game = Game(myself, other_players)
 
-def test_three():
-    players = []
-    for n in ['a', 'b', 'c', 'd', 'e', 'f']:
-        players.append(CluePlayer(n, 3))
-
-    game = ClueGame(players)
-
-    msg = game.record_show('a', CluePersonCard.COLONEL_MUSTARD, 
-            ClueWeaponCard.ROPE, ClueRoomCard.BILLIARD_ROOM)
-
-    assert msg == "Player a has shown"
+    assert len(game.players) > 2
