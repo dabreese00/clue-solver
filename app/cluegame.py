92da3eaf1d6a6a9cd965572b76e2ead3739f2f06
David Breese <dabreese00@gmail.com>
2020-09-13 15:58:29 -0500

    Improve docs and add general querying mechanism
    Continue adding and improving docstrings for backend methods.
    
    Add a querying mechanism, ClueRelationFilter, to hopefully help simplify
    the code of the Game class, and allow external querying, e.g. by the
    frontend.  (This is really stuff that would come for "free" if we
    converted to SQL-based models, but that's not my goal right now.)

--- app/cluegame.py
+++ app/cluegame.py
@@ -86,6 +86,11 @@ class Have:
         return "Have(Player={}, Card={}, {})".format(
                 self.player, self.card, self.yesno)
 
+    def __contains__(self, obj):
+        return obj == self.player \
+            or obj == self.card \
+            or obj == self.card.card_type
+
 
 # A 4-way relationship between a player and 3 cards.
 # Created when a player shows a card to someone else.
@@ -112,19 +117,35 @@ class Show:
         return "Show(Player={}, Cards={}, Void={})".format(
                 self.player, self.cards, self.is_void)
 
+    def __contains__(self, obj):
+        if self.is_void:
+            return False
+        return obj == self.player \
+            or obj in self.cards
+
 
 class Game:
     """Encapsulates and updates the total state of the game knowledge.
 
-    The Game knows a set of Players and a set of Cards, and provides methods to
-    record any and all Shows and Haves amongst them (which also get stored
-    within the game), while simultaneously checking for, and recording the
-    consequences of, any relevant deductions that follow from each new record.
+    The Game instance knows the following:
+        - a set of Cards (self.cards)
+        - a set of Players (self.players)
+        - which Player represents the user (self.myself)
 
-    The Game also necessarily knows which Player is the user herself.
+    The Game instance also keeps track of all known relationships between the
+    Players and the Cards -- in the form of Haves, and Shows.  (See README.md
+    for a more detailed explanation of how this models a Clue game.)
 
-    Last but not least, the Game keeps track of which Cards are "in the file",
-    meaning the Clue confidential file -- this is how you win folks!
+    Game provides methods to record Haves and Shows as they become known, which
+    also recursively check for and record the consequences of any deductions
+    that follow from each newly-discovered relationship.
+
+    And the Game instance keeps track of which Cards are "in the file", meaning
+    the Clue confidential file -- this is how you win folks!
+
+    There are also methods to query the above state (which are also used
+    internally by the recording methods to make deductions), and
+    save/load/delete methods to handle persisting the game state.
 
     Public methods:
         record_have
@@ -147,6 +168,7 @@ class Game:
         haves
         shows
     """
+
     clue_cards = {
         ClueCardType.PERSON: {
             "Colonel Mustard",
@@ -175,22 +197,52 @@ class Game:
     }
 
     def __init__(self, myself, other_players):
-        self.myself = myself
-        self.other_players = other_players
-        self.players = other_players.copy()
-        self.players.add(myself)
+        """Initializes the game state.
 
+        Creates a set of Players and a set of Cards, including which Player is
+        the user.  All other instance variables are initialized to empty.
+
+        Note: Even though the contents of the user's own hand would typically
+        be already known at game start, too, this set of Player-Card
+        relationships is currently recorded separately, after game
+        initialization, using the input_hand method.
+
+        Arguments:
+            myself -- a Player representing the user
+            other_players -- a list of all other Players in the Game
+        """
+
+        # Setup the Cards
         self.cards = set()
         for t in ClueCardType:
             for c in self.clue_cards[t]:
                 self.cards.add(Card(c, t))
 
-        self.cards_in_the_file = set()
+        # Setup the Players
+        self.myself = myself
+        self.other_players = other_players
+        self.players = other_players.copy()
+        self.players.add(myself)
 
+        for p in self.players:
+            for c in self.cards:
+                if p.name == c.name:
+                    raise ValueError(
+                        "Forbidden Player name: {}".format(p.name))
+
+        # Setup the Game state knowledge
         self.haves = []
         self.shows = []
+        self.cards_in_the_file = set()
 
     def record_have(self, player, card, yesno):
+        """Record a Have relationship; make and record any related deductions.
+
+        Arguments:
+            player -- the player for the Have; see Have class
+            card   -- the card for the Have; see Have class
+            yesno  -- the True/False of the Have; see Have class
+        """
 
         prospective_have = Have(player, card, yesno)
 
@@ -262,6 +314,12 @@ class Game:
                     self.cards_in_the_file.add(c)
 
     def record_show(self, player, cards):
+        """Record a Show relationship; make and record any related deductions.
+
+        Arguments:
+            player -- the player for the Show; see Show class
+            cards  -- the cards for the Show; see Show class
+        """
         new_show = Show(player, cards)
         self.shows.append(new_show)
 
@@ -280,8 +338,8 @@ class Game:
             show.is_void = True
             self.record_have(show.player, show.cards.pop(), True)
 
-    # Return the known cards a player has, or does not have.
     def player_known_cards(self, player, yesno):
+        """Returns the known Cards a Player has, or does not have."""
         cards = set()
         for h in self.haves:
             if h.player == player and h.yesno == yesno:
@@ -289,6 +347,7 @@ class Game:
         return cards
 
     def input_hand(self, cards):
+        """Records 3 Cards as belonging to the self.myself Player."""
         for c in cards:
             self.record_have(self.myself, c, True)
 
@@ -297,22 +356,26 @@ class Game:
                 self.record_have(self.myself, c, False)
 
     def get_card(self, name):
+        """Returns a Card object from the Game's list, by name."""
         for c in self.cards:
             if c.name == name:
                 return c
         raise ValueError("No such card! {}".format(name))
 
     def get_player(self, name):
+        """Returns a Player object from the Game's list, by name."""
         for p in self.players:
             if p.name == name:
                 return p
         raise ValueError("No such player! {}".format(name))
 
     def save(self, path):
+        """Persists the Game state to a file."""
         with open(path, 'wb') as dbfile:
             pickle.dump(self, dbfile)
 
     def load(obj, path):
+        """Loads a persisted Game state from a file."""
         if os.path.isfile(path):
             with open(path, 'rb') as dbfile:
                 return pickle.load(dbfile)
@@ -320,8 +383,52 @@ class Game:
             return None
 
     def delete(obj, path):
+        """Deletes a persisted Game state (or any file, really; be careful)."""
         os.remove(path)
 
 
 Game.load = classmethod(Game.load)
 Game.delete = classmethod(Game.delete)
+
+
+# TODO: Make this class immutable?
+class ClueRelationFilter:
+    """Represents a filter to match Clue relations.
+
+    Public methods:
+        match      -- check a single relation against the filter
+        add        -- add a filter condition to the query using "and"
+        get        -- query a list and return results based on the filter
+    """
+    def __init__(self, statement="all"):
+        self.left = None
+        self.right = None
+        self.statement = statement
+
+    def match(self, relation):
+        """Recursively check if statement(s) are in relation."""
+        if self.statement == "and":
+            return self.left.match(relation) and self.right.match(relation)
+        elif self.statement == "or":
+            return self.left.match(relation) or self.right.match(relation)
+        elif self.statement == "all":
+            return True
+        elif self.statement == "have":
+            return relation.yesno
+        elif self.statement == "pass":
+            return not relation.yesno
+        else:
+            return self.statement in relation
+
+    def add(self, op="and", statement=None):
+        new = ClueRelationFilter(op)
+        new.left = self
+        new.right = ClueRelationFilter(statement)
+        return new
+
+    def get(self, relations_list):
+        matching_relations = []
+        for r in relations_list:
+            if self.match(r):
+                matching_relations.append(r)
+        return matching_relations
---
+++ tests/test_cluerelationfilter.py
@@ -0,0 +1,106 @@
+from app.cluegame import (ClueCardType, Card, Player, Have, Show,
+                          ClueRelationFilter)
+
+clue_cards = {
+    ClueCardType.PERSON: {
+        "Colonel Mustard",
+        "Miss Scarlet",
+        "Professor Plum",
+        "Mrs. White",
+        "Mr. Green",
+        "Mrs. Peacock"
+    },
+    ClueCardType.WEAPON: {
+        "Rope",
+        "Lead Pipe",
+        "Revolver",
+        "Candlestick",
+        "Knife",
+        "Wrench"
+    },
+    ClueCardType.ROOM: {
+        "Billiard Room",
+        "Ballroom",
+        "Lounge",
+        "Kitchen",
+        "Conservatory",
+        "Library",
+        "Study"
+    }
+}
+
+
+def get_obj(obj_list, name):
+    for o in obj_list:
+        if o.name == name:
+            return o
+
+
+def test_one():
+
+    hand_size = 3
+    player_names = {'Adam', 'Cynthia', 'Greg', 'David'}
+    players = set()
+    for p in player_names:
+        players.add(Player(p, hand_size))
+
+    cards = set()
+    for t in ClueCardType:
+        for c in clue_cards[t]:
+            cards.add(Card(c, t))
+
+    haves = []
+    haves.append(Have(
+        get_obj(players, "Cynthia"),
+        get_obj(cards, "Billiard Room"),
+        True))
+    haves.append(Have(
+        get_obj(players, "David"),
+        get_obj(cards, "Billiard Room"),
+        False))
+    haves.append(Have(
+        get_obj(players, "Cynthia"),
+        get_obj(cards, "Rope"),
+        True))
+    haves.append(Have(
+        get_obj(players, "Cynthia"),
+        get_obj(cards, "Study"),
+        False))
+
+    shows = []
+    shows.append(Show(
+        get_obj(players, "Cynthia"),
+        [
+            get_obj(cards, "Billiard Room"),
+            get_obj(cards, "Rope"),
+            get_obj(cards, "Study")
+        ]))
+    shows.append(Show(
+        get_obj(players, "David"),
+        [
+            get_obj(cards, "Mrs. White"),
+            get_obj(cards, "Rope"),
+            get_obj(cards, "Study")
+        ]))
+    shows.append(Show(
+        get_obj(players, "Adam"),
+        [
+            get_obj(cards, "Billiard Room"),
+            get_obj(cards, "Rope"),
+            get_obj(cards, "Study")
+        ]))
+
+    filter1 = ClueRelationFilter(get_obj(players, "Cynthia"))
+    filter2 = filter1.add("and", get_obj(cards, "Billiard Room"))
+    filter3 = filter2.add("and", get_obj(cards, "Rope"))
+
+    filter4 = filter1.add("or", get_obj(cards, "Billiard Room"))
+
+    assert len(filter1.get(haves)) == 3
+    assert len(filter2.get(haves)) == 1
+    assert len(filter3.get(haves)) == 0
+    assert len(filter4.get(haves)) == 4
+    assert len(filter1.get(shows)) == 1
+    assert len(filter2.get(shows)) == 1
+    assert len(filter3.get(shows)) == 1
+    assert len(filter4.get(shows)) == 2
