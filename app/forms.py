f0012a5b92968a2d8bb40f1255c2b5f75b0340b4
David Breese <dabreese00@gmail.com>
2020-09-21 22:49:05 -0500

    Add TODO comments for frontend validation

--- app/forms.py
+++ app/forms.py
@@ -12,30 +12,38 @@ class CreateGameForm(FlaskForm):
     player = FieldList(FormField(CreatePlayerForm),
                        min_entries=6, max_entries=6, label="Players")
     submit = SubmitField()
+    # TODO: Validate at least 2 players.
+    # TODO: Validate that total player hand sizes add up to # cards.
 
 
 class InputHandForm(FlaskForm):
     myself = SelectField('Myself')
     cards = SelectMultipleField('My cards')
     submit = SubmitField()
+    # TODO: Validate cards selected equal to player hand size.
 
 
 class InputPassForm(FlaskForm):
     player = SelectField('Player')
     cards = FieldList(SelectField('Cards'), min_entries=3, max_entries=3)
     submit_pass = SubmitField('Record pass')
+    # TODO: Validate person, weapon, room
+    # TODO: Validate logically possible
 
 
 class InputShowForm(FlaskForm):
     player = SelectField('Player')
     cards = FieldList(SelectField('Cards'), min_entries=3, max_entries=3)
     submit_show = SubmitField('Record show')
+    # TODO: Validate person, weapon, room
+    # TODO: Validate logically possible
 
 
 class InputRevealForm(FlaskForm):
     player = SelectField('Player')
     card = SelectField('Card')
     submit_reveal = SubmitField('Record reveal')
+    # TODO: Validate logically possible
 
 
 class DeleteGameForm(FlaskForm):
